b64e8f11ac73e269f8f0ccfee790ce3969cc7078
JingLin0 <jinglin.daisy@gmail.com>
2020-02-19 15:37:19 +0100

    setup file and singer tap template

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-capterra",
+    version="0.1.0",
+    description="Singer.io tap for extracting data from Capterra",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_capterra"],
+    install_requires=["singer-python>=5.0.12", "requests",],
+    entry_points="""
+    [console_scripts]
+    tap-capterra=tap_capterra:main
+    """,
+    packages=["tap_capterra"],
+    package_data={"schemas": ["tap_capterra/schemas/*.json"]},
+    include_package_data=True,
+)
