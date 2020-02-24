7e20eb8783fe5d23919324d2934b60b3650810e3
JingLin0 <jinglin.daisy@gmail.com>
2020-02-24 15:32:08 +0100

    discover catalog

--- tap_capterra/__init__.py
+++ tap_capterra/__init__.py
@@ -28,42 +28,28 @@ def discover():
     streams = []
 
     for tap_stream_id, props in STREAMS.items():
-        stream_key_properties = props.get("key_properties")
+        key_properties = props.get("key_properties")
         mdata = metadata.get_standard_metadata(
             schema=schema,
-            key_properties=stream_key_properties,
+            key_properties=key_properties,
             valid_replication_keys=STREAMS.get("valid_replication_keys"),
         )
-
-        catalog_entry = {
-            "stream": tap_stream_id,
-            "tap_stream_id": tap_stream_id,
-            "schema": schema,
-            "metadata": mdata,
-            "key_properties": stream_key_properties,
-        }
-        streams.append(catalog_entry)
-
-    return {"streams": streams}
-
-
-def get_selected_streams(catalog):
-    """
-    Gets selected streams.  Checks schema's 'selected' first (legacy)
-    and then checks metadata (current), looking for an empty breadcrumb
-    and mdata with a 'selected' entry
-    """
-    selected_streams = []
-    for stream in catalog["streams"]:
-        stream_metadata = metadata.to_map(stream["metadata"])
-        # stream metadata will have an empty breadcrumb
-        if metadata.get(stream_metadata, (), "selected"):
-            selected_streams.append(stream["tap_stream_id"])
-
-    return selected_streams
+        streams.append(
+            CatalogEntry(
+                stream=tap_stream_id,
+                tap_stream_id=tap_stream_id,
+                key_properties=key_properties,
+                schema=Schema.from_dict(schema),
+                metadata=mdata,
+            )
+        )
+    return Catalog(streams)
 
 
-def sync(config, state, catalog):
+def do_discover():
+    catalog = discover()
+    catalog_dict = catalog.to_dict()
+    json.dump(catalog_dict, sys.stdout, indent="  ", sort_keys=True)
 
     selected_stream_ids = get_selected_streams(catalog)
 
@@ -83,11 +69,8 @@ def main():
     # Parse command line arguments
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-    # If discover flag was passed, run discovery mode and dump output to stdout
     if args.discover:
-        catalog = discover()
-        print(json.dumps(catalog, indent=2))
-    # Otherwise run in sync mode
+        do_discover()
     else:
         if args.catalog:
             catalog = args.catalog
