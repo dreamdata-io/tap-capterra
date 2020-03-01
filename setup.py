ab56d2857902ab289e53e9eec6385f4f8b6a79f9
JingLin0 <jinglin.daisy@gmail.com>
2020-03-01 15:22:42 +0100

    fix up

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     name="tap-capterra",
     version="0.1.0",
     description="Singer.io tap for extracting data from Capterra",
-    author="Stitch",
-    url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_capterra"],
     install_requires=["singer-python>=5.0.12", "requests", "ratelimit", "tqdm"],
